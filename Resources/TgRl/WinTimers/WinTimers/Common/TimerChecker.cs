94db1a45fa70f5b7c67399bc189e0c9563b157b0
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2022-02-06 13:38:40 +0300

    TgRl

--- Resources/TgRl/WinTimers/WinTimers/Common/TimerChecker.cs
+++ Resources/TgRl/WinTimers/WinTimers/Common/TimerChecker.cs
@@ -9,20 +9,8 @@ namespace Common
 {
     public static class TimerChecker
     {
-        private record Result(
-            int TimesCount,
-            long First,
-            long Min,
-            long Max,
-            double Average,
-            double GoodPercent,
-            double AboveGood,
-            double? AverageAboveGood);
-
         private static readonly TimeSpan MeasurementDuration = TimeSpan.FromMinutes(3);
         private static readonly int[] IntervalsToCheck = { 1, 10, 100 };
-
-        private const int GoodAreaSize = 5;
 
         public static void Check(ITimer timer)
         {
@@ -33,21 +21,13 @@ namespace Common
             foreach (var intervalMs in IntervalsToCheck)
             {
                 Console.WriteLine($"Measuring interval of {intervalMs} ms...");
-                
-                var result = CheckInterval(timer, intervalMs);
-
-                Console.WriteLine($"Results on {result.TimesCount} times:");
-                Console.WriteLine($"    first      = {result.First}");
-                Console.WriteLine($"    min/max    = {result.Min}/{result.Max}");
-                Console.WriteLine($"    average    = {result.Average:0.##}");
-                Console.WriteLine($"    good       = {result.GoodPercent:0.##} %");
-                Console.WriteLine($"    above good = {result.AboveGood:0.##} % (average {result.AverageAboveGood:0.##})");
+                MeasureInterval(timer, intervalMs);
             }
 
             Console.WriteLine("All done.");
         }
 
-        private static Result CheckInterval(ITimer timer, int intervalMs)
+        private static void MeasureInterval(ITimer timer, int intervalMs)
         {
             var times = new List<long>((int)Math.Round(MeasurementDuration.TotalMilliseconds) + 1);
             var stopwatch = new Stopwatch();
@@ -72,28 +52,6 @@ namespace Common
             }
 
             File.WriteAllLines($"deltas_{intervalMs}.txt", deltas.Select(d => d.ToString()));
-
-            var min = deltas.Min();
-            var max = deltas.Max();
-            var average = deltas.Average();
-
-            double GetPercent(Func<long, bool> selector) =>
-                deltas.Count(selector) / (double)deltas.Count * 100;
-
-            var goodPercent = GetPercent(d => d >= intervalMs - GoodAreaSize && d <= intervalMs + GoodAreaSize);
-            var aboveGoodPercent = GetPercent(d => d > intervalMs + GoodAreaSize);
-            var aboveGood = deltas.Where(d => d > intervalMs + GoodAreaSize).ToArray();
-            var averageAboveGood = aboveGood.Any() ? (double?)aboveGood.Average() : null;
-
-            return new Result(
-                times.Count,
-                times[0],
-                min,
-                max,
-                average,
-                goodPercent,
-                aboveGoodPercent,
-                averageAboveGood);
         }
     }
 }
