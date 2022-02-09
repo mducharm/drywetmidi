98ba68b9b9720bbc229bdacc4ee43db1ec1a69ba
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2022-02-09 19:02:26 +0300

    TgRl

--- Resources/TgRl/WinTimers/WinTimers/Common/TimerChecker.cs
+++ Resources/TgRl/WinTimers/WinTimers/Common/TimerChecker.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
+using System.Timers;
 
 namespace Common
 {
@@ -16,6 +17,7 @@ namespace Common
         {
             Console.WriteLine("Starting measuring...");
             Console.WriteLine($"OS: {Environment.OSVersion}");
+            Console.WriteLine($"CPUs: {Environment.ProcessorCount}");
             Console.WriteLine("--------------------------------");
 
             foreach (var intervalMs in IntervalsToCheck)
@@ -30,26 +32,32 @@ namespace Common
         private static void CheckInterval(ITimer timer, int intervalMs)
         {
             var times = new List<long>((int)Math.Round(MeasurementDuration.TotalMilliseconds) + 1);
-            var cpuUsage = new List<float>((int)Math.Round(MeasurementDuration.TotalMilliseconds) + 1);
+            var cpuUsage = new List<int>((int)Math.Round(MeasurementDuration.TotalMilliseconds) + 1);
             
             var stopwatch = new Stopwatch();
             var cpuCounter = new PerformanceCounter("Process", "% Processor Time", Process.GetCurrentProcess().ProcessName);
-            
-            Action callback = () =>
+            var cpuTimer = new System.Timers.Timer(1000);
+            var cpuStep = 0;
+            cpuTimer.Elapsed += (_, _) =>
             {
-                times.Add(stopwatch.ElapsedMilliseconds);
-                cpuUsage.Add(cpuCounter.NextValue());
+                var value = cpuCounter.NextValue();
+                if (cpuStep++ % 2 != 0)
+                    cpuUsage.Add((int)Math.Round(value / Environment.ProcessorCount));
             };
             
+            Action callback = () => times.Add(stopwatch.ElapsedMilliseconds);
+
             timer.Start(intervalMs, callback);
+            cpuTimer.Start();
             stopwatch.Start();
 
             Thread.Sleep(MeasurementDuration);
 
             timer.Stop();
             stopwatch.Stop();
+            cpuTimer.Stop();
             
-            File.WriteAllLines($"cpu_{intervalMs}.txt", cpuUsage.Select(u => u.ToString()));
+            File.WriteAllLines($"cpu_{intervalMs}.txt", cpuUsage.ToArray().Select(u => u.ToString()));
 
             var deltas = new List<long>();
             var lastTime = 0L;
