88871b3eb855e6240f5ecb2b25d4326d57fad865
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2022-02-18 20:02:27 +0300

    TgRl

---
+++ Resources/TgRl/MacTimers/MacTimers/InfiniteLoopTimerWithSleepPrioritized/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using Common;
+
+namespace InfiniteLoopTimerWithSleepPrioritized
+{
+    public sealed class Timer : ITimer
+    {
+        private bool _running;
+
+        public void Start(int intervalMs, Action callback)
+        {
+            var thread = new Thread(() =>
+            {
+                _running = true;
+
+                while (_running)
+                {
+                    Thread.Sleep(intervalMs);
+                    callback();
+                }
+            })
+            { Priority = ThreadPriority.Highest };
+
+            thread.Start();
+        }
+
+        public void Stop()
+        {
+            _running = false;
+        }
+    }
+}
